a356d79dcd931285aec7d991a8d8a727633635c4
sechvn <63130885+sechvn@users.noreply.github.com>
2020-12-12 16:21:38 -0500

    Add files via upload
    This is a program that creates a checking and savings account using functions to deposit, transfer, or withdraw funds. There isn't any error handling yet but will be working on improving the code. Thanks goes to Michael my tutor and also my brother Nick G. for encouraging and helping me. My tutor is an amazing teacher who I found through Fiverr. He did a tremendous job in helping me problem solve with code and to think about things differently.

---
+++ bankathomefinal.py
@@ -0,0 +1,58 @@
+# Eric Gimbel
+savings = 1000
+checking = 1000
+
+def currentAmount(savings, checking):
+	print(f"Current Savings Amount: {savings}\nCurrent Checking Amount: {checking}" )
+	user = input('Do you want to deposit, transfer, or withdraw: ')
+	account = input('Which account do you want to choose to complete this transaction: ')
+	
+	if user == 'deposit':
+		depositMoney(savings, checking, account)
+
+	elif user == 'transfer':
+		transfer(savings, checking, account)
+
+	elif user == 'withdraw':
+		withdrawMoney(savings, checking, account)
+
+	else:		
+		print('Please choose one of the options')
+		currentAmount(savings, checking)
+
+def transfer(savings, checking, account):
+	user = float(input('How much money would you like to tranfer: '))
+	if account == 'savings':
+		checking -= user
+		savings += user
+		currentAmount(savings, checking)
+	elif account == 'checking':
+		savings -= user
+		checking += user
+		currentAmount(savings, checking)
+
+def depositMoney(savings, checking, account):
+	user = float(input('How much money would you like to deposit: '))
+	if account == 'savings':
+		savings += user
+		currentAmount(savings, checking)
+	elif account == 'checking':
+		checking += user
+		currentAmount(savings, checking)
+
+def withdrawMoney(savings, checking, account):
+	user = float(input('How much money do you wish to withdraw: '))
+	if account == 'savings':
+		savings -= user
+		currentAmount(savings, checking)
+	elif account == 'checking':
+		checking -= user
+		currentAmount(savings, checking)
+
+
+currentAmount(savings, checking)
+
+
+
+
+
