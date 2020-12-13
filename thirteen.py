8235c05346754343998a131c584359abcba5a2de
sechvn <63130885+sechvn@users.noreply.github.com>
2020-12-12 20:12:07 -0500

    Update thirteen.py

--- thirteen.py
+++ thirteen.py
@@ -1,22 +1,23 @@
-import random
+#Eric Gimbel Tony Gladdis Random Number Guessing Game
 
-def randomNumber():
+import random                 # Import the random module
+
+def randomNumber():			  # Function that creates range of random numbers.
 	ranNum = random.randint(1, 100)
 	return ranNum
 
-def userinput(message = "Please guess a random number: "):
+def userinput(message = "Please guess a random number: "):	# Function that passes two arguments with a message that can be changed after calling different function.
 	askForNumber = int(input(message))
 	return askForNumber
 
-def userNumber(askForNumber, ranNum):
-	if askForNumber > ranNum:
-		return "Too high"
-	elif askForNumber < ranNum:
-		return "Too low"
+def userNumber(askForNumber, ranNum):	#Function that tests whether random number is too high or too low.
+	while askForNumber > ranNum:
+		return "Too High"
+	if askForNumber < ranNum:
+		return "Too Low"
 	else:
 		return "Congratulations! You guessed correctly."
-
-def main():
+def main():								#Function that declares boolean values then tests using while loop and allows the game to continue.
 	userWins = False
 	beginGuess = True
 
@@ -36,5 +37,5 @@ def main():
 main()
 
 
-
+# I used this as my main source and reference: https://www.youtube.com/watch?v=xDqkMqjBZiE&t=12s
 
