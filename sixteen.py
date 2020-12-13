3eebbf603bd3007ba3c0afab6048abaaad321430
sechvn <63130885+sechvn@users.noreply.github.com>
2020-12-12 20:10:33 -0500

    Update sixteen.py

--- sixteen.py
+++ sixteen.py
@@ -1,35 +1,39 @@
-# Eric Gimbel 12/8/20 Derby Racecar
+# Eric Gimbel 12/8/20 Derby Racecar 
 
-def main():
+outfile = open('pinewood.txt', 'w')
 
-	outfile = open('pinewood.txt', 'w')
+onetime = float(input('Please enter the first race time of your Derby race: '))
+twotime = float(input('Please enter the second race time of your Derby race: '))
+thirdtime = float(input('Please enter the third race time of your Derby race: '))
+fourthtime = float(input('Please enter the fourth race time of your Derby race: '))
 
-	onetime = float(input('Please enter the first race time of your Derby race: '))
-	twotime = float(input('Please enter the second race time of your Derby race: '))
-	thirdtime = float(input('Please enter the third race time of your Derby race: '))
-	fourthtime = float(input('Please enter the fourth race time of your Derby race: '))
+outfile.write(str(onetime) + '\n')
+outfile.write(str(twotime) + '\n')
+outfile.write(str(thirdtime) + '\n')
+outfile.write(str(fourthtime) + '\n')
 
-	outfile.write(str(onetime) + '\n')
-	outfile.write(str(twotime) + '\n')
-	outfile.write(str(thirdtime) + '\n')
-	outfile.write(str(fourthtime) + '\n')
+outfile.close()
+list1 = [onetime, twotime, thirdtime, fourthtime]
 
-	outfile.close()
+def adjustTime(x):
+	x.sort()
+	x.pop(0)
+	average = sum(x) / len(x)
+	return average
 
-	infile = open('pinewood.txt', 'r')
 
-	onetime = float(infile.readline())
-	twotime = float(infile.readline())
-	thirdtime = float(infile.readline())
-	fourthtime = float(infile.readline())
+print(adjustTime(list1))
 
-	infile.close()
 
-	average = onetime + twotime + thirdtime + fourthtime
+
+
+
+
+
+
 	
 
 
-main()
 
 
 
